327963cbb51e81ffd462e6d24e9a12ed817e388c
Arjun <arjunsinha200428@gmail.com>
2022-04-10 13:46:13 +0200

    Fixed all warnings

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {Component} from "react";
 
 const styledField = {
     width: '200px',
     background: "#343434"
 }
 
+class Field extends Component {
+    render() {
+        const holder = 'enter here'
+        return <input
+            style={styledField}
+            type='text'
+            placeholder={holder}/>
+    }
+}
+
 const Header = () => {
     return <h2>Ass We can</h2>
 }
-const Field = () => {
-    const holder = 'enter here'
+//const Field = () => {
+  //  const holder = 'enter here'
 
-    return <input 
-        style={styledField}
-        type='text'
-        placeholder={holder}/>
-}
+//     return <input
+//         style={styledField}
+//         type='text'
+//         placeholder={holder}/>
+// }
 
 function Btn(){
     const logIn = "Log In"
--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {styledField, Btn} from "./App";
 
-const intorpolation = "Hello Ass"
 
 const rootElement = document.getElementById('root');
 
 const root = createRoot(rootElement);
-
-
-const Elem = (
-    <>
-        <input style={styledField} type="text" placeholder="ass"/>
-    </>
-
-)
 root.render(
     <>
         <App />
