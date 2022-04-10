8e2fb850b98260acdd701cabad7a37032f220845
Arjun <arjunsinha200428@gmail.com>
2022-04-10 13:23:28 +0200

    Small changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
+const styledField = {
+    width: '200px',
+    background: "#343434"
+}
+
+const Header = () => {
+    return <h2>Ass We can</h2>
+}
+const Field = () => {
+    const holder = 'enter here'
+
+    return <input 
+        style={styledField}
+        type='text'
+        placeholder={holder}/>
+}
+
+function Btn(){
+    const logIn = "Log In"
+    const logged = true
+    return <button>{logged ? "enter" : logIn}</button>
+}
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+        <Header/>
+        <Field/>
+        <Btn/>
     </div>
   );
 }
 
+export {styledField, Btn}
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {styledField, Btn} from "./App";
 
-const elem = <h2>Hello World</h2>
-const ekem = React.createElement('h2', /*class*/{className: "greetings"},/* Definnetion*/'Witaj')
+const intorpolation = "Hello Ass"
 
-ReactDOM.render(
-  // <React.StrictMode>
-  //   <App />
-  //
-  // </React.StrictMode>,
-    ekem,
-  document.getElementById('root')
+const rootElement = document.getElementById('root');
+
+const root = createRoot(rootElement);
+
+
+const Elem = (
+    <>
+        <input style={styledField} type="text" placeholder="ass"/>
+    </>
+
+)
+root.render(
+    <>
+        <App />
+    </>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
